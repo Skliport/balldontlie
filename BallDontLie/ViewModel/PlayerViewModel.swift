96fe794ecfa1ab361c479d61f8dda48bc28fef5c
René Francisco Santamaría Calderón <skliport@gmail.com>
2022-02-28 14:52:50 -0600

    🎨 ViewModel added

--- BallDontLie/ViewModel/PlayerViewModel.swift
+++ BallDontLie/ViewModel/PlayerViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import Combine
+
+class PlayerViewModel: ObservableObject {
+    
+    @Published var charactersState:CharacterViewModelState=CharacterViewModelState.initial
+    
+    let DataService: Service=Service()
+    var cancellable = Set<AnyCancellable>()
+    
+    
+    init() {
+        getAllCharacters()
+    }
+    
+    func getAllCharacters(){
+        charactersState=CharacterViewModelState.loading
+        DataService.getAllPlayers()
+            .sink { [weak self] completion in
+                switch completion{
+                    
+                case .finished:
+                    print("finish")
+                case .failure(let error):
+                    self?.charactersState=CharacterViewModelState.error(errorMessage: "\(error)")
+                }
+            } receiveValue: { [weak self] Characters in
+                self?.charactersState=CharacterViewModelState.loaded(userModel: Characters)
+            }
+            .store(in: &cancellable)
+    }
+    
+}
+
